1862bcd9ca5c064f2015e2a46726c239b7ec900d
Daksh Sawhney <darkstardaksh@gmail.com>
2025-08-06 09:18:04 +0530

    Added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import PortfolioForm from './pages/PortfolioForm'
-import PortfolioPreview from './pages/PortfolioPreview'
+import PortfolioForm from './pages/PortfolioForm.jsx'
+import PortfolioPreview from './pages/PortfolioPreview.jsx'
+import TemplateOne from './templates/TemplateOne.jsx'
+import TemplateTwo from './templates/TemplateTwo.jsx'
 
 const App = () => {
   return (
-    <div className=''>
+    <div>
       <Routes>
         <Route path='/' element={<PortfolioForm/>} />
         <Route path='/preview' element={<PortfolioPreview/>} />
+        <Route path='/template1' element={<TemplateOne/>} />
+        <Route path='/template2' element={<TemplateTwo/>} />
       </Routes>
     </div>
   )
